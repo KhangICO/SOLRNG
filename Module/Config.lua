17c5422e26845b667cf0e692a364b70425d9229f
KhangICO <107622734+KhangICO@users.noreply.github.com>
2026-03-31 09:41:49 +0700

    Initialize configuration for egg features
    Add configuration settings for egg features and rarity.

--- Module/Config.lua
+++ Module/Config.lua
@@ -1,1 +1,16 @@
+local cfg = {}
 
+cfg.AutoEgg = false
+cfg.AutoHop = false
+cfg.ShowESP = true
+
+cfg.SelectedEggs = {}
+
+cfg.Rarity = {
+    ["Dreamer Egg"]="0.1%",
+    ["Egg V2.0"]="0.2%",
+    ["Andromeda Egg"]="0.3%",
+    ["Point Egg VI"]="5%"
+}
+
+return cfg
